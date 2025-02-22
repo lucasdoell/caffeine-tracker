ee5fb717abbf9edf7ef50b9ef3b1a79a134bcbe2
Lucas Doell <lucas@lux.dev>
2025-02-22 11:51:34 -0500

    Add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
