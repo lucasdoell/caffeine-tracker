b74d469c382949fc682893f444b1917a31784676
Lucas Doell <lucas@lux.dev>
2025-02-22 16:05:30 -0500

    Remove separator

--- frontend/src/dashboard/quick-selector-card.tsx
+++ frontend/src/dashboard/quick-selector-card.tsx
@@ -6,7 +6,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { PlusIcon } from "lucide-react";
 
 export function QuickSelectorCard() {
@@ -30,10 +29,8 @@ export function QuickSelectorCard() {
               </div>
             </div>
 
-            <Separator orientation="vertical" />
-
             {/* Selector */}
-            <div className="flex items-center gap-2">
+            <div className="items-center space-y-2">
               <img
                 src={
                   "https://images.unsplash.com/photo-1588483977150-9c2127ab7bcc?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
