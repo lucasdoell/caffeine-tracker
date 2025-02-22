3415f923be8fa26ec1ced5108f226ea7c218ca60
Lucas Doell <lucas@lux.dev>
2025-02-22 15:18:03 -0500

    Update dashboard

--- frontend/src/dashboard/page.tsx
+++ frontend/src/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { CaffeineChart } from "@/components/charts/caffeine-chart";
+import { Card, CardHeader } from "@/components/ui/card";
 
 function DashboardLayout({ children }: React.PropsWithChildren) {
   return (
@@ -12,7 +14,18 @@ function DashboardLayout({ children }: React.PropsWithChildren) {
 export function DashboardPage() {
   return (
     <DashboardLayout>
-      <main></main>
+      <main className="flex flex-col gap-6 p-6">
+        <Card>
+          <CardHeader>
+            <h1 className="text-2xl">Dashboard</h1>
+          </CardHeader>
+          <section className="grid gap-6 p-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="gap-6">
+              <CaffeineChart />
+            </div>
+          </section>
+        </Card>
+      </main>
     </DashboardLayout>
   );
 }
