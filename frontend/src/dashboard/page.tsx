db302b42036a179674e4ebf5a06a04086827f028
Lucas Doell <lucas@lux.dev>
2025-02-22 15:42:07 -0500

    Add quick selector card

--- frontend/src/dashboard/page.tsx
+++ frontend/src/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { CaffeineChart } from "@/components/charts/caffeine-chart";
+import { CaffeineOverTimeChart } from "@/components/charts/caffeine-over-time";
 import { Card, CardHeader } from "@/components/ui/card";
+import { QuickSelectorCard } from "@/dashboard/quick-selector-card";
 
 function DashboardLayout({ children }: React.PropsWithChildren) {
   return (
@@ -22,6 +24,12 @@ export function DashboardPage() {
           <section className="grid gap-6 p-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             <div className="gap-6">
               <CaffeineChart />
+            </div>
+            <div className="gap-6 col-span-full xl:col-span-3">
+              <CaffeineOverTimeChart />
+            </div>
+            <div className="gap-6 col-span-2">
+              <QuickSelectorCard />
             </div>
           </section>
         </Card>
---
+++ frontend/src/dashboard/quick-selector-card.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { PlusIcon } from "lucide-react";
+
+export function QuickSelectorCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Quick Add</CardTitle>
+        <CardContent>
+          <div className="flex flex-row gap-2 items-center space-x-4 text-sm">
+            {/* Plus button */}
+            <div className="flex items-center gap-2">
+              <div className="flex flex-col gap-2">
+                <ul>
+                  <li>Caffeine</li>
+                  <li>Water</li>
+                  <li>Energy</li>
+                </ul>
+                <Button variant="outline">
+                  <PlusIcon className="h-4 w-4" /> Add new
+                </Button>
+              </div>
+            </div>
+
+            <Separator orientation="vertical" />
+
+            {/* Selector */}
+            <div className="flex items-center gap-2">
+              <img
+                src={
+                  "https://images.unsplash.com/photo-1588483977150-9c2127ab7bcc?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+                alt="placeholder"
+                className="h-32 w-32 rounded"
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </CardHeader>
+    </Card>
+  );
+}
