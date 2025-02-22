f8ca10292c1645eff4269134c1e087eb3ed3e390
Lucas Doell <lucas@lux.dev>
2025-02-22 17:53:40 -0500

    Add w-full

--- frontend/src/dashboard/page.tsx
+++ frontend/src/dashboard/page.tsx
@@ -42,7 +42,7 @@ export function DashboardPage() {
 
   return (
     <DashboardLayout>
-      <main className="flex flex-col gap-6 p-6">
+      <main className="flex flex-col gap-6 p-6 w-full">
         <Card>
           <CardHeader>
             <h1 className="text-2xl">Dashboard</h1>
@@ -63,14 +63,14 @@ export function DashboardPage() {
               </CardContent>
             </Card>
           </section>
-          <section className="grid gap-6 p-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <section className="grid gap-6 p-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
             <div className="gap-6">
               <CaffeineChart />
             </div>
             <div className="gap-6">
               <CaffeineLog />
             </div>
-            <div className="gap-6 col-span-3">
+            <div className="gap-6 col-span-full xl:col-span-2">
               <CaffeineOverTimeChart />
             </div>
             <div className="gap-6 col-span-2">
