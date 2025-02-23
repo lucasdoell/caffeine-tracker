384a216ac1501ebefa900035cc46afe00d2d9e70
Adeeb <adeeb19909@gmail.com>
2025-02-23 00:32:00 -0500

    added caffeine remaining endpoint

--- backend/caffeine/urls.py
+++ backend/caffeine/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import CaffeineLogCreateAPIView, CaffeineLogListAPIView, CaffeineLogDetailAPIView
+from .views import CaffeineLogCreateAPIView, CaffeineLogListAPIView, CaffeineLogDetailAPIView, CaffeineOverTimeAPIView
 
 urlpatterns = [
     path('logs/', CaffeineLogListAPIView.as_view(), name='caffeine-log-list'),  # List all logs
     path('logs/create/', CaffeineLogCreateAPIView.as_view(), name='caffeine-log-create'),  # Create log
     path('logs/<uuid:pk>/', CaffeineLogDetailAPIView.as_view(), name='caffeine-log-detail'),  # Retrieve single log by UUID
+    path('caffeine-over-time/', CaffeineOverTimeAPIView.as_view(), name='caffeine-over-time'), 
 ]
+
+
