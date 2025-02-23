6e4dbdbf0684bffc193f1b6253fe542a803de323
Lucas Doell <lucas@lux.dev>
2025-02-22 21:41:30 -0500

    Proxy requests through vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,30 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.API_URL,
-        changeOrigin: true,
-        secure: false,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
