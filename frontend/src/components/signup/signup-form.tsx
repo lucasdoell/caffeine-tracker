64b639b105efcd1bc2fc70a96752e630991f45d6
Adeeb <adeeb19909@gmail.com>
2025-02-25 00:29:57 -0500

    modifying errors

--- frontend/src/components/signup/signup-form.tsx
+++ frontend/src/components/signup/signup-form.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { SignUpResponse } from "@/types/auth";
+//import { SignUpResponse } from "@/types/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
